e71bb270fa891a5f8c5db02742334b9fbc13e6c0
Jura Skrlec <jura.skrlec@microblink.com>
2020-08-24 12:37:33 +0200

    Renamed class for iOS form BlinkId to blinkIdCapacitorPlugin

--- BlinkID/ios/Plugin/Plugin.m
+++ BlinkID/ios/Plugin/Plugin.m
@@ -3,6 +3,6 @@
 
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
-CAP_PLUGIN(BlinkId, "BlinkId",
+CAP_PLUGIN(BlinkIdCapacitorPlugin, "BlinkIdCapacitorPlugin",
            CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
 )
--- BlinkID/ios/Plugin/Plugin.swift
+++ BlinkID/ios/Plugin/Plugin.swift
@@ -5,8 +5,8 @@ import Capacitor
  * Please read the Capacitor iOS Plugin Development Guide
  * here: https://capacitorjs.com/docs/plugins/ios
  */
-@objc(BlinkId)
-public class BlinkId: CAPPlugin {
+@objc(BlinkIdCapacitorPlugin)
+public class BlinkIdCapacitorPlugin: CAPPlugin {
 
     @objc func echo(_ call: CAPPluginCall) {
         let value = call.getString("value") ?? ""
