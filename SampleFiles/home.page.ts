b1a513ccb34fb926ee58ff69809d868575e3acd8
sandra_kuzmic <kuzmic.sandra@microblink.com>
2020-08-27 14:35:06 +0200

    update how plugin is called and add android licence

--- SampleFiles/home.page.ts
+++ SampleFiles/home.page.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 import { Plugins } from '@capacitor/core';
 import * as BlinkID from 'blinkid-capacitor';
-import { BlinkIDPlugin } from 'blinkid-capacitor';
+
+const { BlinkIDPlugin } = Plugins;
 
 @Component({
   selector: 'app-home',
@@ -14,22 +15,21 @@ export class HomePage {
   constructor() {}
 
   async scan() {
-    const blinkIdPlugin = new BlinkIDPlugin();
 
-    var blinkIdCombinedRecognizer = new BlinkID.BlinkIdCombinedRecognizer();
+    const blinkIdCombinedRecognizer = new BlinkID.BlinkIdCombinedRecognizer();
     blinkIdCombinedRecognizer.returnFullDocumentImage = true;
     blinkIdCombinedRecognizer.returnFaceImage = true;
 
-    const licenseKeys: BlinkID.License = {
+    const licenseKeys: BlinkID.Licenses = {
       ios: 'sRwAAAEQaW8uaW9uaWMuc3RhcnRlctOOXHJ8cZk7ufDmqgoofWxXk8xN56gLofT/CuVhdpQ6cM4+FALHNqjvQIhC4SVx6Bhr+cAtPXGqUUSlgHsnHFF184jqfj2pS3h/t88v05xzvRuqllhRIRED63t6VoYar0uq20hydzt3Z56O9RcNL2MxeAVl+A6cI9L0yVAOKJp9zCrbt8dyz78bOyptp+ZK5Gr8czSlNyENh8vePEF1lkwCfvZ09jHIO9wzJ8RTWb8Ag61H8YkDfkhncxPTHP9M5tnZUYBHVL8C',
-      android: 'sRwAAAAWY29tLmJsaW5raWRyZWFjdG5hdGl2ZYouOuuUS2CbdVuoF2lsSqeObkzyko/0kah+gpMcK/G18fQtB1vAoiuyDojRyY6xOLlJYAFwGuepBKDJPqdsQh11RxKSLANRLDH0lWJzgo3EKRae9NXAoKPUJ0nqUy7uIP8eAVkFelNILVRy3pkWJjY5o/vRLn7sf2cNsdmt/ZvzUw1545pLHgT7LqOxSRg+81p0/koApvVMnJ+yI9QJgHNwa9AFNDOYEKCiow/J1dJjx+yGL1crnvfaOLv7ndLUisX496v+w9k=',
+      android: 'sRwAAAAQaW8uaW9uaWMuc3RhcnRlcllSTd5TI1Nktn18BfZbW8EcY04UttzTXWKQ+BBbEEqnsoDp9umwexkqKqa2+FSaripQtlblOMrfE9SUSguKIMfQ2ZVVECXNQHyi8y7yoe/swzU9qZqCymqXFsN0Xs2BfqhIimw5OPWYQ2WqeXM7FFrFsygKIhXWlz0GuK3BGMNH08CD8kG6plPIYu+0NMPkd17lYnmRor9oWuzG6QR9tLoYb1FNYMa/SLIlJ2ctQ/PzTiE7o0J3VtWgNUMpfm9VdfQ7gDABrfqFWcMzgrApb2/O6+nGMbmUq2NP9aosFVgXmFBPYr/afQ5keFPyeLu0HFPNYeP77SZYvuKwt/GUlXd6OOQKPmr10Gv+HJh98d+j/pZmy7A4EXSGiBqe00fQKtn9eTp2FUQEhr2216wCbxoQ7EjXN16CV/2pTaCV/LKLcBLSSs420K0ttI0XAnilcBj3SoYD0NkM3nZoMk60h1FVTtyC2EF9h9ObERY6lhVbeX/UFYP/pDTlwmJOeOHD',
       showTimeLimitedLicenseKeyWarning: true
-    }
+    };
 
-    const scanningResults = await blinkIdPlugin.scanWithCamera(
-      new BlinkID.BlinkIdOverlaySettings(),
-      new BlinkID.RecognizerCollection([blinkIdCombinedRecognizer/*, mrtdSuccessFrameGrabber*/]),
-      licenseKeys
-    );
+    const scanningResults = await BlinkIDPlugin.scanWithCamera({
+      overlaySettings: new BlinkID.BlinkIdOverlaySettings(),
+      recognizerCollection: new BlinkID.RecognizerCollection([blinkIdCombinedRecognizer/*, mrtdSuccessFrameGrabber*/]),
+      licenses: licenseKeys
+    });
   }
 }
