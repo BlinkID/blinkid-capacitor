415951d29f7c8aaf667cf78de8631fc10132e352
sandra_kuzmic <kuzmic.sandra@microblink.com>
2020-08-31 11:45:12 +0200

    update scanner/index with check for scan cancelled

--- BlinkID/src/scanner/index.ts
+++ BlinkID/src/scanner/index.ts
@@ -19,21 +19,24 @@ export enum ScanningStatus {
 export class BlinkIDPlugin implements BlinkIDPluginInterface {
   async scanWithCamera(overlaySettings: OverlaySettings, recognizerCollection: RecognizerCollection, license: License): Promise<any> {
 
-    let functions = []
+    let functions = [];
     for ( let recognizer of recognizerCollection.recognizerArray ) {
         let recognizerFunction = recognizer.createResultFromNative;
         functions.push( recognizerFunction );
         delete recognizer.createResultFromNative;
     }
 
-    const response = await BlinkIDCapacitorPlugin.scanWithCamera({'overlaySettings': overlaySettings, 'recognizerCollection': recognizerCollection, 'license': license})
+    const response = await BlinkIDCapacitorPlugin.scanWithCamera({'overlaySettings': overlaySettings, 'recognizerCollection': recognizerCollection, 'license': license});
 	const results = response.resultList;
+    const isCancelled = response.cancelled;
 	let resultsFromNative = [];
-    for ( let i = 0; i < results.length; ++i ) {
-        recognizerCollection.recognizerArray[ i ].createResultFromNative = functions[ i ];
-		let result = recognizerCollection.recognizerArray[i].createResultFromNative(results[i]);
-		if (result.resultState != RecognizerResultState.empty) {
-        	resultsFromNative.push(result);
+	if (!isCancelled && results) {
+        for ( let i = 0; i < results.length; ++i ) {
+            recognizerCollection.recognizerArray[ i ].createResultFromNative = functions[ i ];
+            let result = recognizerCollection.recognizerArray[i].createResultFromNative(results[i]);
+            if (result.resultState != RecognizerResultState.empty) {
+                resultsFromNative.push(result);
+            }
         }
     }
 
